68c3549f00dc65122d74b1c08b488072e56e61c1
Michael Osterlie <michael.osterlie@nexteraanalytics.com>
2022-08-26 17:06:48 -0500

    Initial work on mixed Kotlin/JS impl

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,23 +1,12 @@
 import trello.CardButton
 
 fun main() {
-
-}
-
-fun cardButtons(t: Any, options: Any) : CardButton {
-    return CardButton("", "Test Button") {}
-}
-
-fun Map<String, Any>.toJs(): dynamic {
-    val result: dynamic = object {}
-
-    for ((key, value) in this) {
-        when (value) {
-            is String -> result[key] = value
-            is Function<*> -> result[key] = value
-            else -> throw RuntimeException("value has invalid type")
-        }
-    }
-
-    return result
+    js("""TrelloPowerUp.initialize({
+        'card-buttons': function(t, options){
+            return [{
+                icon: '/static/icon-dark.png',
+                text: 'Estimate Size',
+            }];
+        },
+    })""")
 }
---
+++ src/main/kotlin/neurallink/NeuralLinkPowerUp.kt
@@ -0,0 +1,12 @@
+package neurallink
+
+/**
+ * Entry point for all Neural Link functionality. This will be the only real class as every else will be based on
+ * Functional Programming.
+ */
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+@JsName("NeuralLink")
+class NeuralLinkPowerUp {
+
+}
