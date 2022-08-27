aa50b55892cbb4519bb5277d831e94f95a2c0d50
Michael Osterlie <github@outerrim.org>
2022-08-26 23:46:23 -0500

    Added some logging to debug the callback

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,13 +1,16 @@
 fun main() {
     js("""var nl = require('neural-link-trello');
-        console.log("nl created, calling initialize()");
+        console.log("nl created, calling initialize()", nl);
         TrelloPowerUp.initialize({
             'card-buttons': function(t, options) {
                 console.log("card-buttons capability")
                 return [{
                     icon: '/static/icon-dark.png',
                     text: 'Estimate Size',
-                    callback: function(t) { nl.cardButtons(t); }
+                    callback: function(t) {
+                        console.log("callback nl", nl);
+                        nl.cardButtons(t); 
+                    }
                 }];
             },
         });""")
