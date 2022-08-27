ea6ae7f0319212f45ce0e4b9ed2130b0429322c3
Michael Osterlie <github@outerrim.org>
2022-08-26 23:17:41 -0500

    Added log message

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,7 +1,8 @@
 fun main() {
     js("""var nl = require('neural-link-trello');
         TrelloPowerUp.initialize({
-            'card-buttons': function(t, options){
+            'card-buttons': function(t, options) {
+                console.log("card-buttons capability")
                 return [{
                     icon: '/static/icon-dark.png',
                     text: 'Estimate Size',
